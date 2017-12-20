143398cd70be5f671b2bdd9bab4d24b9a1d132f1
Kesus Kim <kesuskim@gmail.com>
2017-12-20 14:08:05 +0900

    Add MySQL variable assignments

--- dump.go
+++ dump.go
@@ -23,12 +23,23 @@ type dump struct {
 	CompleteTime  string
 }
 
-const version = "0.2.0"
+const version = "0.2.1"
 
 const tmpl = `-- Go SQL Dump {{ .DumpVersion }}
 --
 -- ------------------------------------------------------
 -- Server version	{{ .ServerVersion }}
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 {{range .Tables}}
@@ -37,16 +48,22 @@ const tmpl = `-- Go SQL Dump {{ .DumpVersion }}
 --
 
 DROP TABLE IF EXISTS {{ .Name }};
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 {{ .SQL }};
-{{ if .Values }}
+/*!40101 SET character_set_client = @saved_cs_client */;
 --
 -- Dumping data for table {{ .Name }}
 --
 
 LOCK TABLES {{ .Name }} WRITE;
+/*!40000 ALTER TABLE {{ .Name }} DISABLE KEYS */;
+{{ if .Values }}
 INSERT INTO {{ .Name }} VALUES {{ .Values }};
+{{ end }}
+/*!40000 ALTER TABLE {{ .Name }} ENABLE KEYS */;
 UNLOCK TABLES;
-{{end}}{{ end }}
+{{ end }}
 -- Dump completed on {{ .CompleteTime }}
 `
 
