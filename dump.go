846720a32178575d8fd73301f6b5bc25334a1320
Brandon Roehl <brandon.roehl@jamf.com>
2019-01-23 14:59:24 -0600

    Update to MySQL 8

--- dump.go
+++ dump.go
@@ -38,7 +38,7 @@ const headerTmpl = `-- Go SQL Dump {{ .DumpVersion }}
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ SET NAMES utf8mb4;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -52,20 +52,21 @@ const tableTmpl = `
 -- Table structure for table {{ .Name }}
 --
 
-DROP TABLE IF EXISTS {{ .Name }};
+DROP TABLE IF EXISTS ` + "`{{ .Name }}`" + `;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4;
 {{ .SQL }};
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 --
 -- Dumping data for table {{ .Name }}
 --
 
 LOCK TABLES {{ .Name }} WRITE;
 /*!40000 ALTER TABLE {{ .Name }} DISABLE KEYS */;
-{{ if .Values }}
+{{- if .Values }}
 INSERT INTO {{ .Name }} VALUES {{ .Values }};
-{{ end }}
+{{- end }}
 /*!40000 ALTER TABLE {{ .Name }} ENABLE KEYS */;
 UNLOCK TABLES;
 `
--- dump_test.go
+++ dump_test.go
@@ -298,7 +298,7 @@ func TestDumpOk(t *testing.T) {
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ SET NAMES utf8mb4;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -306,26 +306,23 @@ func TestDumpOk(t *testing.T) {
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
-
 --
 -- Table structure for table Test_Table
 --
 
-DROP TABLE IF EXISTS Test_Table;
+DROP TABLE IF EXISTS \Test_Table\;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4;
 CREATE TABLE 'Test_Table' (\id\ int(11) NOT NULL AUTO_INCREMENT,\email\ char(60) DEFAULT NULL, \name\ char(60), PRIMARY KEY (\id\))ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 --
 -- Dumping data for table Test_Table
 --
 
 LOCK TABLES Test_Table WRITE;
 /*!40000 ALTER TABLE Test_Table DISABLE KEYS */;
-
 INSERT INTO Test_Table VALUES ('1',null,'Test Name 1'),('2','test2@test.de','Test Name 2');
-
 /*!40000 ALTER TABLE Test_Table ENABLE KEYS */;
 UNLOCK TABLES;
 
