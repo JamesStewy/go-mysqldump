196010f6f23d0d91a32157265b29eb3976fb0d8a
S.Kağan Cin <cin_kagan@hotmail.com>
2021-04-12 08:38:18 +0300

    Update dump.go

--- dump.go
+++ dump.go
@@ -66,40 +66,6 @@ COMMIT;
 -- Dump completed on {{ .CompleteTime }}
 `
 
-const tmpl = `-- Go SQL Dump {{ .DumpVersion }}
---
--- ------------------------------------------------------
--- Server version	{{ .ServerVersion }}
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-{{range .Tables}}
---
--- Table structure for table {{ .Name }}
---
-
-DROP TABLE IF EXISTS {{ .Name }};
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-{{ .SQL }};
-/*!40101 SET character_set_client = @saved_cs_client */;
---
--- Dumping data for table {{ .Name }}
---
-{{ end }}
--- Dump completed on {{ .CompleteTime }}
-`
-
 func contains(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
@@ -152,19 +118,10 @@ func (d *Dumper) Dump(withData bool, tableNames []string) (string, error) {
 			return p, err
 		}
 	}
-
 	// Set complete time
 	data.CompleteTime = time.Now().String()
 
-	// Write dump to file
-	var tmplSchema string
-	if withData || contains(tableNames, name) {
-		tmplSchema = tmplWithData
-	} else {
-		tmplSchema = tmpl
-	}
-
-	t, err := template.New("mysqldump").Parse(tmplSchema)
+	t, err := template.New("mysqldump").Parse(tmplWithData)
 	if err != nil {
 		return p, err
 	}
@@ -213,7 +170,8 @@ func createTable(db *sql.DB, name string, withData bool, tableNames []string) (*
 	}
 
 	if withData || contains(tableNames, name) {
-		if t.Values, err = createTableValues(db, name); err != nil {
+		t.Values, err = createTableValues(db, name)
+		if err != nil {
 			return nil, err
 		}
 	}
